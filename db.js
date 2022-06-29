d40a03a78366dfd2ebe1e625ef41dc265fb73d7c
dolyprh <dolyprahoro@gmail.com>
2022-06-29 20:46:31 +0700

    update api

--- db.js
+++ db.js
@@ -1,15 +1,10 @@
-// const items = require("./data/items");
-// const tags = require("./data/tags");
-// const brands = require("./data/brands");
-// const itemTypes = require("./data/itemTypes");
 const article = require("./data/article");
-
+const edukasi = require("./data/edukasi");
+const data = require("./data/data.json")
 module.exports = {
-  // items,
-  // tags,
-  // brands,
-  // itemTypes,
-  article
+  article,
+  edukasi,
+  data
 }
 
 
